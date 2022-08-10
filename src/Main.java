353ce5a5eef1171b0d4de2c90c4595402fdba9c7
grand <ferthlin@ya.ru>
2022-08-10 20:26:55 +0300

    исправила дз 5 по замечаниям задание 4

--- src/Main.java
+++ src/Main.java
@@ -61,9 +61,7 @@ public class Main {
         //Задание 4
         for (int i = 0; i < array1.length; i++) {
             if (array1[i]%2!=0){
-                int evenNumber = array1[i];
-                evenNumber++;
-                array1[i] = evenNumber;
+                array1[i]++;
             }
             System.out.println(array1[i]);
 
